9fddfd34333d7caf78fee1d14a5f904a64ccc912
Alf <alfgoto@gmail.com>
2024-03-15 11:45:00 +0100

    fixation fixed

--- script.js
+++ script.js
@@ -1,31 +1,77 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+var videoARR = ['']
+
+
+document.addEventListener('DOMContentLoaded', () => {
 
 
     let plan3D = document.getElementById('plan3D')
     let cubesARR = document.getElementsByClassName('cube')
 
 
-    document.querySelector('#un .top').addEventListener('click', ()=>{
-        // document.getElementById('visit').remove()
+    document.querySelector('#un .top').addEventListener('click', () => {
+        document.getElementById('visit').remove()
         plan3D.classList.add('start')
 
-        setTimeout(()=>{
+        setTimeout(() => {
             plan3D.classList.add('rotate')
+            let previousCube = 8
+            let newCube = 0
 
-            cubesARR[0].classList.add('selectedCube')
-            let previousCube = 0
-
-            for(let i = 0; i < cubesARR.length; i++){
-                cubesARR[i].addEventListener('click', ()=>{
+            let time = 0
+            var interval = window.setInterval(() => {
+                if (time++ == 4) { clearInterval(interval) }
+                if (newCube != previousCube) {
                     cubesARR[previousCube].classList.remove('selectedCube')
-                    cubesARR[i].classList.add('selectedCube')
-                    console.log((i * 10 + 20) + 'svw')
+                    cubesARR[newCube].classList.add('selectedCube')
+                    previousCube = newCube
+
+                    createVideo(document.querySelector(".selectedCube ." + timeToSide(time-1)))
+                } else {
+                    cubesARR[previousCube].classList.remove('selectedCube')
+                    setTimeout(() => { cubesARR[previousCube].classList.add('selectedCube') }, 0.1)
+                }
+            }, 5000)
+            plan3D.addEventListener('animationend', () => {
+                plan3D.classList.remove('rotate')
+                setTimeout(() => { plan3D.classList.add('rotate') }, 0.1)
+
+                let time = 0
+                var interval = window.setInterval(() => {
+                    if (time++ == 4) { clearInterval(interval) }
+                    if (newCube != previousCube) {
+                        cubesARR[previousCube].classList.remove('selectedCube')
+                        cubesARR[newCube].classList.add('selectedCube')
+                        previousCube = newCube
+
+                        createVideo(document.querySelector(".selectedCube ." + timeToSide(time-1)))
+                    }
+                }, 5000)
+            })
+
+            for (let i = 0; i < cubesARR.length; i++) {
+                cubesARR[i].addEventListener('click', () => {
+                    newCube = i
                     document.getElementById('plan3D').style.top = ((i * 10 - 10) * -1) + 'svw'
-                    previousCube = i
                 })
             }
-
         }, 3000)
     })
 
 })
+
+function createVideo(dom){
+    let index = dom.parentElement.getAttribute('pos')
+}
+
+function timeToSide(time) {
+    switch (time) {
+        case 1:
+            return 'back'
+        case 2:
+            return 'left'
+        case 3:
+            return 'front'
+        default:
+            return 'right'
+    }
+}
