312d6586f8f00aa07fe259ddda104feadb0e70be
Alf <alfgoto@gmail.com>
2024-03-15 13:33:55 +0100

    first two videos added

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-var videoARR = ['']
+class video{
+    constructor(name, explanation, languages){
+        this.name = name
+        this.path = 'video/' + name + '.mp4'
+        this.explanation = explanation
+        this.languages = languages
+    }
+}
+
+
+var videoARR = [
+    new video('minesweeper', 'This is my first website, i did it to learn all the basics. It is a minesweeper game done in particular using the websocket for the fastest players', ['Javascript', 'PHP', 'MYSQL', 'CSS', 'HTML']), 
+    new video('fenetreS', 'This is a little project done with the curiosity to see if it was possible', ['Javascript', 'CSS', 'HTML'])
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
 
     let plan3D = document.getElementById('plan3D')
     let cubesARR = document.getElementsByClassName('cube')
@@ -25,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     cubesARR[newCube].classList.add('selectedCube')
                     previousCube = newCube
 
-                    createVideo(document.querySelector(".selectedCube ." + timeToSide(time-1)))
+                    createVideo(document.querySelector(".selectedCube ." + timeToSide(time - 1)))
                 } else {
                     cubesARR[previousCube].classList.remove('selectedCube')
                     setTimeout(() => { cubesARR[previousCube].classList.add('selectedCube') }, 0.1)
@@ -43,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         cubesARR[newCube].classList.add('selectedCube')
                         previousCube = newCube
 
-                        createVideo(document.querySelector(".selectedCube ." + timeToSide(time-1)))
+                        createVideo(document.querySelector(".selectedCube ." + timeToSide(time - 1)))
                     }
                 }, 5000)
             })
@@ -59,8 +90,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 })
 
-function createVideo(dom){
+function createVideo(dom) {
+    console.log('video creation')
+    if (document.getElementsByClassName('video')[0] != null) { document.getElementsByClassName('video')[0].remove() }
     let index = dom.parentElement.getAttribute('pos')
+    let video = document.createElement('video')
+    video.classList.add('video')
+    video.muted = true
+    video.controls = true
+    video.autoplay = true
+    dom.appendChild(video)
+    video.src = videoARR[index].path
 }
 
 function timeToSide(time) {
@@ -75,3 +115,4 @@ function timeToSide(time) {
             return 'right'
     }
 }
+
